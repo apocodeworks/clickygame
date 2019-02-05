b651e3709f04c4ae70456b636747d8401252fef1
Brett O'Connell <apocreativeworks@gmail.com>
2019-02-04 19:31:52 -0600

    Updates

---
+++ precache-manifest.35b1d44e62bcf6a635a6e1688557a4d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "81f23c200282511c2871",
+    "url": "/clickygame/static/js/main.81f23c20.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/clickygame/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "81f23c200282511c2871",
+    "url": "/clickygame/static/css/main.b2832d16.chunk.css"
+  },
+  {
+    "revision": "c7fffdeb9ba7ba0bde7b8be9d36e365e",
+    "url": "/clickygame/index.html"
+  }
+];
